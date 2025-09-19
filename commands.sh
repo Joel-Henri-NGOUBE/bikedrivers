92743e004d1c99d0631e4178e3f2ae088aebf276
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-09-19 20:32:48 +0200

    Test Deployment -9

--- commands.sh
+++ commands.sh
@@ -1,5 +1,4 @@
-echo "Bonjour"
-echo "Bonsoir"
+echo "$MYSQL_USER"
 php bin/console doctrine:database:create --if-not-exists
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
 php bin/console make:migration
