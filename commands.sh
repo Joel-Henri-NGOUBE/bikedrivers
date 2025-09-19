4ccbae49dd29465a519d9ef1909b463a6dca8417
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-09-19 20:35:50 +0200

    Test Deployment -10

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-echo "$MYSQL_USER"
+echo "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/bikedrivers_api?serverVersion=8.0.32&charset=utf8mb4"
 php bin/console doctrine:database:create --if-not-exists
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
 php bin/console make:migration
