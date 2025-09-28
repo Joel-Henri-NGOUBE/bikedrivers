256f9a90f5e7d2ba0eb0c7fd174e8baa10c28a49
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-09-29 00:04:29 +0200

    Backup configuration

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-sleep 150
+sleep 90
 php bin/console doctrine:database:create --if-not-exists
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
 while true; do php bin/console make:migration && break; done
